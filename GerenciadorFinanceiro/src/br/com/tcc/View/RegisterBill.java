3b9395146649f19bb6b56de433ab75b9bded596e
cainan <csomaster@gmail.com>
2011-09-13 23:24:20 -0300

    Implementando um datePicker.

--- GerenciadorFinanceiro/src/br/com/tcc/View/RegisterBill.java
+++ GerenciadorFinanceiro/src/br/com/tcc/View/RegisterBill.java
@@ -1,11 +1,26 @@
 package br.com.tcc.View;
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import br.com.tcc.R;
 
 public class RegisterBill extends Activity {
+
+    private static final int DATE_DIALOG_ID = 0;
+
+    // date and time
+    private int mYear;
+    private int mMonth;
+    private int mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +32,52 @@ public class RegisterBill extends Activity {
 
         setContentView(R.layout.register_bill);
 
+        initView();
+
+        Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
     }
+
+    private void initView() {
+        EditText vencimentoEdit = (EditText) findViewById(R.id.vencimento_edit);
+        vencimentoEdit.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                showDialog(DATE_DIALOG_ID);
+            }
+
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+        case DATE_DIALOG_ID:
+            return new DatePickerDialog(this, mDateSetListener, mYear, mMonth, mDay);
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPrepareDialog(int id, Dialog dialog) {
+        switch (id) {
+        case DATE_DIALOG_ID:
+            ((DatePickerDialog) dialog).updateDate(mYear, mMonth, mDay);
+            break;
+        }
+    }
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            mYear = year;
+            mMonth = monthOfYear;
+            mDay = dayOfMonth;
+            // updateDisplay();
+        }
+    };
 
 }
