59ac94d94c9a1b2fdd5c7ae090426e0c23e54491
ueslei <ueslei182@yahoo.com.br>
2011-09-16 11:25:22 -0300

    Edited GerenciadorFinanceiro/src/br/com/tcc/Location/locationManager.java via GitHub

--- GerenciadorFinanceiro/src/br/com/tcc/Location/locationManager.java
+++ GerenciadorFinanceiro/src/br/com/tcc/Location/locationManager.java
@@ -1,47 +1,14 @@
 package br.com.tcc.Location;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Criteria;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationManager;
+import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.provider.Settings;
 import android.widget.Toast;
-import android.content.Context;
-import android.content.ContextWrapper;
 
-public class locationManager {
+public class gpsManager {
 	
-	private LocationManager locationManager;
-	private Location location;
-	
-
-	
-	public String getNameLocation(Context ctx){
-		
-		locationManager = (LocationManager) ctx.getSystemService(ctx.LOCATION_SERVICE);
-        String bestProvider = locationManager.getBestProvider(new Criteria(),true);
-        location = locationManager.getLastKnownLocation(bestProvider);
-		
-		Geocoder geoCoder = new Geocoder(ctx, Locale.getDefault());
-		String end = null;
-		
-        try {
-            List<Address> addresses = geoCoder.getFromLocation(location.getLatitude(),location.getLongitude(), 5);
-            if (addresses.size() > 0) {
-                end = addresses.get(0).getFeatureName();    	                  	                
-            }    
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return end;
-	}
 	
 	public void turnGPSOn(Context ctx){
         String provider = Settings.Secure.getString(ctx.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
@@ -66,17 +33,27 @@ public class locationManager {
             ctx.sendBroadcast(poke);
         }
     }
-    
-    public void chkGpsStatus(Context ctx){
+	
+	
+	
+	public boolean chkConnectionStatus(Context ctx){
     	
-    	LocationManager locationManager = (LocationManager) ctx.getSystemService(ctx.LOCATION_SERVICE);
-		boolean isGPS = locationManager.isProviderEnabled (LocationManager.GPS_PROVIDER);
-		
-		if(isGPS)
-			Toast.makeText(((ContextWrapper) ctx).getBaseContext(),"GPS is ON", Toast.LENGTH_SHORT).show();
-		else			
-			Toast.makeText(((ContextWrapper) ctx).getBaseContext(),"GPS is OFF", Toast.LENGTH_SHORT).show();	 	
+    	ConnectivityManager connMgr = (ConnectivityManager)
+    	ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+    	final android.net.NetworkInfo wifi = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+    	final android.net.NetworkInfo mobile = connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+    	if(wifi.isAvailable()){
+    		Toast.makeText(ctx, "Wifi Connection" , Toast.LENGTH_LONG).show();
+    		return true;
+    	}
+    	else if( mobile.isAvailable() ){
+    		Toast.makeText(ctx, "Mobile 3G Connection" , Toast.LENGTH_LONG).show();
+    		return true;
+    	}
+    	else{
+    		Toast.makeText(ctx, "No Network Connection" , Toast.LENGTH_LONG).show();
+    		return false;
+    		}
     }
-
-
 }
