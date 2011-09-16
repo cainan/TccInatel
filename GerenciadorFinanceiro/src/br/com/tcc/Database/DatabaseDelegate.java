0e9d9b0f85a6b784e122baf885e0fa2e42160240
cainan <csomaster@gmail.com>
2011-09-15 21:13:21 -0300

    creating data base.

--- GerenciadorFinanceiro/src/br/com/tcc/Database/DatabaseDelegate.java
+++ GerenciadorFinanceiro/src/br/com/tcc/Database/DatabaseDelegate.java
@@ -1,37 +1,73 @@
 package br.com.tcc.Database;
 
-import br.com.tcc.Model.Conta;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import br.com.tcc.Model.Conta;
 
 public class DatabaseDelegate {
-	
-	private SQLiteDatabase dbGerFinanceiro;  
-	
-	private static final String SCRIPT_DB_DELETE = "DROP TABLE IF EXISTS contas";  
-    private static final String SCRIPT_DB_CREATE = "create table contas ( );";  
-  
-    public DatabaseDelegate (Context ctx){  
-        SQLiteHelper dbHelper = new SQLiteHelper(ctx, "dbGerFinanceiro", 1, SCRIPT_DB_CREATE, SCRIPT_DB_DELETE);  
-        dbGerFinanceiro = dbHelper.getWritableDatabase();  
-    }  
-  
-	private long inserir(Conta c){  
-    	ContentValues cv = new ContentValues();  
-        cv.put("nome", c.getNome());  
-        cv.put("valor", c.getValor());  
+
+    private SQLiteDatabase dbGerFinanceiro;
+
+    private static final String DB_NAME = "Gerenciador_Financeiro";
+    private static final int DB_VERSION = 1;
+    private static final String TABLE_NAME = "Contas";
+
+    private static final String SCRIPT_DB_DELETE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    private static final String SCRIPT_DB_CREATE = "create table " + TABLE_NAME
+            + "( _id integer primary key autoincrement," + "conta text not null,"
+            + "valor text not null," + "vencimento text not null," + "codigo text,"
+            + "status integer not null);";
+
+    private SQLiteHelper mDatabaseHelper;
+
+    private static DatabaseDelegate mDatabaseDelegate;
+
+    public static DatabaseDelegate getInstance(Context applicationContext) {
+        // TODO Auto-generated method stub
+        if (mDatabaseDelegate == null) {
+            mDatabaseDelegate = new DatabaseDelegate(applicationContext);
+        }
+
+        return mDatabaseDelegate;
+    }
+
+    public DatabaseDelegate(Context ctx) {
+        if (mDatabaseHelper == null) {
+            mDatabaseHelper = new SQLiteHelper(ctx, DB_NAME, DB_VERSION, SCRIPT_DB_CREATE,
+                    SCRIPT_DB_DELETE);
+            dbGerFinanceiro = mDatabaseHelper.getWritableDatabase();
+
+        }
+    }
+
+    public void closeDb() {
+        if (mDatabaseHelper != null) {
+            mDatabaseHelper.close();
+        }
+    }
+
+    public synchronized long inserir(Conta c) {
+        ContentValues cv = new ContentValues();
+
+        cv.put("conta", c.getNome());
+        cv.put("valor", c.getValor());
         cv.put("vencimento", c.getVencimento());
-        cv.put("codigoBarras", c.getCodigoBarra());
-        
-        return dbGerFinanceiro.insert("contas", null, cv);
-    }  
-  
-//    private long atualizar(Conta bill){  
-//
-//    }  
-//  
-//    public int excluir(int id){  
-//          
-//    }  
+        cv.put("codigo", c.getCodigoBarra());
+        cv.put("status", c.isPago());
+
+        Long success;
+        success = dbGerFinanceiro.insert(TABLE_NAME, null, cv);
+
+        closeDb();
+        return success;
+    }
+
+    // private long atualizar(Conta bill){
+    //
+    // }
+    //  
+    // public int excluir(int id){
+    //          
+    // }
 }
--- GerenciadorFinanceiro/src/br/com/tcc/Database/SQLiteHelper.java
+++ GerenciadorFinanceiro/src/br/com/tcc/Database/SQLiteHelper.java
@@ -3,28 +3,28 @@ package br.com.tcc.Database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
-public class SQLiteHelper extends SQLiteOpenHelper {  
-    private String scriptCreate;  
-    private String scriptDelete;  
-  
-    public SQLiteHelper(Context ctx, String nomeBd,   
-        int versaoBanco, String scriptCreate,   
-        String scriptDelete) {  
-  
-        super(ctx, nomeBd, null, versaoBanco);  
-        this.scriptCreate = scriptCreate;  
-        this.scriptDelete = scriptDelete;  
-    }  
-  
-    public void onCreate(SQLiteDatabase db) {  
-        db.execSQL(scriptCreate);  
-    }  
-  
-    public void onUpgrade(SQLiteDatabase db,   
-        int oldVersion, int newVersion) {  
-  
-        db.execSQL(scriptDelete);  
-        onCreate(db);  
-    }  
-} 
+public class SQLiteHelper extends SQLiteOpenHelper {
+    private String scriptCreate;
+    private String scriptDelete;
+
+    public SQLiteHelper(Context ctx, String nomeBd, int versaoBanco, String scriptCreate,
+            String scriptDelete) {
+
+        super(ctx, nomeBd, null, versaoBanco);
+        this.scriptCreate = scriptCreate;
+        this.scriptDelete = scriptDelete;
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        Log.d("log", "creating DB");
+        db.execSQL(scriptCreate);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d("log", "upgrading DB");
+        db.execSQL(scriptDelete);
+        onCreate(db);
+    }
+}
