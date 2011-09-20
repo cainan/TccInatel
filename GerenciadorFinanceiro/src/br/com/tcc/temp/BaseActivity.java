c6feb41ece2f7ea10054db3040dd9441dab9db2f
cainan <csomaster@gmail.com>
2011-09-19 21:38:36 -0300

    Primeiro passo para correção do problema com o nome dos pacotes;

---
+++ GerenciadorFinanceiro/src/br/com/tcc/temp/BaseActivity.java
@@ -0,0 +1,49 @@
+package br.com.tcc.temp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+import br.com.tcc.utils.GpsManager;
+
+public class BaseActivity extends Activity {
+
+    /** Hold an instance of GPS manager */
+    public GpsManager mGpsManager = new GpsManager();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    /**
+     * Turn the gps off
+     */
+    @Override
+    protected void onResume() {
+        this.mGpsManager.turnGPSOff(getApplicationContext());
+        super.onResume();
+    }
+
+    /**
+     * Open Google maps to search for banks
+     */
+    public void searchBanksGmaps() {
+        if (mGpsManager.chkConnectionStatus(getApplicationContext())) {
+            mGpsManager.turnGPSOn(getApplicationContext());
+            String geoUriString = ("geo:0,0?q=Agencias bancarias");
+            Uri geoUri = Uri.parse(geoUriString);
+            Intent mapCall = new Intent(Intent.ACTION_VIEW, geoUri);
+            startActivity(mapCall);
+        } else
+            Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_LONG)
+                    .show();
+    }
+
+}
---
+++ GerenciadorFinanceiro/src/br/com/tcc/temp/NotificationActivity.java
@@ -0,0 +1,116 @@
+package br.com.tcc.temp;
+
+import java.util.GregorianCalendar;
+
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import br.com.tcc.R;
+
+public class NotificationActivity extends BaseActivity {
+
+    /** Hold the Bundle parameter of bill */
+    public static final String BILL_PARAMETER = "billParameter";
+
+    /** Hold the Notification Id */
+    private int mBillId;
+
+    /** Hold the Bill received */
+    private Conta mBill;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.decision_view);
+
+        // Get the intent sent from notification
+        Intent itt = getIntent();
+        if (itt != null) {
+            mBill = (Conta) itt.getSerializableExtra(BILL_PARAMETER);
+            mBillId = mBill.getId();
+        }
+
+        initView();
+    }
+
+    /**
+     * Initializing the view
+     */
+    private void initView() {
+        Button searchBankBtn = (Button) findViewById(R.id.search_bank);
+        searchBankBtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                searchBanksGmaps();
+            }
+
+        });
+
+        Button markAsPayedBtn = (Button) findViewById(R.id.mark_as_payed);
+        markAsPayedBtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                DatabaseDelegate.getInstance(getApplicationContext()).markAsPayed(mBillId);
+                finish();
+            }
+
+        });
+
+        Button remindLaterBtn = (Button) findViewById(R.id.remind_later);
+        remindLaterBtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                remindMeLater();
+            }
+
+        });
+
+        Button remarkBtn = (Button) findViewById(R.id.remark);
+        remarkBtn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Remarcar a data da conta
+            }
+
+        });
+    }
+
+    /**
+     * Set the bill's notification field to 15 minutes later
+     */
+    private void remindMeLater() {
+        GregorianCalendar deadLine = new GregorianCalendar();
+        Log.d("log", "deadline: " + deadLine.getTime());
+
+        deadLine.roll(GregorianCalendar.MINUTE, 15);
+        Log.d("log", "deadline: " + deadLine.getTime());
+
+        // TODO Quando a hora for, por exemplo, 10:50, se aumentarmos 15 min, a hora ficará 10:05.
+        // precisa-se de uma lógica para incrementar esses 15 minutos da forma certa.
+
+        finish();
+
+    }
+
+    /**
+     * Cancel the notification at Notification bar when the activity is destroyed
+     */
+    @Override
+    protected void onDestroy() {
+
+        // Cleaning the notification bar
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(mBillId);
+
+        super.onDestroy();
+    }
+
+}
