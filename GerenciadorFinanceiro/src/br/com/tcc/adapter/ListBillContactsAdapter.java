25f5827d79ad691814f1be3fbf0cff162c9ff943
Luiz <lcarlos13@gmail.com>
2011-11-03 17:09:47 -0200

    - Implementação da feature nova de notificar sobre contas e escolher as contas e os contatos para enviar.

---
+++ GerenciadorFinanceiro/src/br/com/tcc/adapter/ListBillContactsAdapter.java
@@ -0,0 +1,119 @@
+package br.com.tcc.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import br.com.tcc.R;
+import br.com.tcc.model.Conta;
+import br.com.tcc.model.Contacts;
+
+public class ListBillContactsAdapter extends BaseAdapter {
+
+	/** Hold the Application Context */
+	private Context mContext;
+
+	/** Hold an array of Contacts to be shown in listView */
+	private List<Contacts> mContacts = new ArrayList<Contacts>();
+
+	private Contacts mContactSelected;
+
+	private List<Contacts> listContactSelected = new ArrayList<Contacts>();
+
+	/**
+	 * The class' constructor
+	 * 
+	 * @param applicationContext
+	 * @param contacts
+	 */
+	public ListBillContactsAdapter(Context applicationContext, List<Contacts> contacts) {
+		mContext = applicationContext;
+		mContacts = contacts;
+	}
+
+	@Override
+	public int getCount() {
+		return mContacts.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return mContacts.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public List<Contacts> getListContactSelected() {
+		return listContactSelected;
+	}
+
+	public void setListContactSelected(List<Contacts> listContactSelected) {
+		this.listContactSelected = listContactSelected;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		RelativeLayout rowLayout = (RelativeLayout) LayoutInflater.from(mContext).inflate(
+				R.layout.list_contacts_bill_item, parent, false);
+
+		if (mContacts != null && mContacts.get(position) != null) {
+
+			// Set list item according to product item data
+			TextView contactName = (TextView) rowLayout.findViewById(R.id.bill_name_contact);
+			if (contactName != null) {
+				contactName.setText(mContacts.get(position).getName());
+			}          
+
+			final CheckBox check = (CheckBox) rowLayout.findViewById(R.id.checkbox_contact);
+
+			if (check != null && mContacts.get(position) != null) {
+				check.setChecked(false);
+			}
+
+			final int pos = position;
+
+			check.setOnClickListener(new OnClickListener() {
+
+				public void onClick(View v) {
+					mContactSelected = mContacts.get(pos);
+					if (check.isChecked()) {
+						listContactSelected.add(mContactSelected);
+						setListContactSelected(listContactSelected);
+					}
+					else {
+						listContactSelected.remove(listContactSelected.indexOf(mContactSelected));
+						setListContactSelected(listContactSelected);
+					}
+				}
+			});
+		}
+
+		return rowLayout;
+	}
+	
+	/**
+     * Update Contacts instace to update the listView
+     * 
+     * @param bills
+     */
+    public void updateAdapter(List<Contacts> contacts) {
+        if (mContacts != null && contacts != null) {
+            mContacts.clear();
+            mContacts = contacts;
+        }
+        notifyDataSetChanged();
+    }
+}
+
---
+++ GerenciadorFinanceiro/src/br/com/tcc/adapter/ListBillPaidAdapter.java
@@ -0,0 +1,134 @@
+package br.com.tcc.adapter;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import br.com.tcc.R;
+import br.com.tcc.model.Conta;
+
+public class ListBillPaidAdapter extends BaseAdapter {
+
+    /** Hold the Application Context */
+    private Context mContext;
+
+    /** Hold an array of Bills to be shown in listView */
+    private ArrayList<Conta> mBillsPaid;
+    
+    private Conta mBillSelected;
+    
+    private List<Conta> listBillSelected = new ArrayList<Conta>();
+
+    /**
+     * The class' constructor
+     * 
+     * @param applicationContext
+     * @param bills
+     */
+    public ListBillPaidAdapter(Context applicationContext, ArrayList<Conta> bills) {
+        mContext = applicationContext;
+        mBillsPaid = bills;
+    }
+
+    @Override
+    public int getCount() {
+        return mBillsPaid.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mBillsPaid.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+   @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        RelativeLayout rowLayout = (RelativeLayout) LayoutInflater.from(mContext).inflate(
+                R.layout.list_bill_paid_item, parent, false);
+
+        if (mBillsPaid != null && mBillsPaid.get(position) != null) {
+
+            // Set list item according to product item data
+            TextView billName = (TextView) rowLayout.findViewById(R.id.bill_name_paid);
+            if (billName != null) {
+                billName.setText(mBillsPaid.get(position).getNome());
+            }
+
+            TextView billDeadline = (TextView) rowLayout.findViewById(R.id.bill_deadline_paid);
+            if (billDeadline != null) {
+                billDeadline.setText(mBillsPaid.get(position).getVencimento());
+            }
+
+            TextView billValue = (TextView) rowLayout.findViewById(R.id.bill_value_paid);
+            if (billValue != null) {
+            	float valor = Float.parseFloat(mBillsPaid.get(position).getValor());
+            	NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+                billValue.setText(nf.format(valor));
+            }
+           
+           
+            final CheckBox check = (CheckBox) rowLayout.findViewById(R.id.checkbox_paid);
+            
+            
+            if (check != null && mBillsPaid.get(position) != null) {
+                check.setChecked(false);
+            }
+            
+            final int pos = position;
+            
+            check.setOnClickListener(new OnClickListener() {
+				
+				public void onClick(View v) {
+					mBillSelected = mBillsPaid.get(pos);
+					if (check.isChecked()) {
+						listBillSelected.add(mBillSelected);
+						setListBillSelected(listBillSelected);
+					}
+					else {
+						listBillSelected.remove(listBillSelected.indexOf(mBillSelected));
+						setListBillSelected(listBillSelected);
+					}
+				}
+			});
+        }
+
+        return rowLayout;
+    }
+
+    /**
+     * Update Bills instace to update the listView
+     * 
+     * @param bills
+     */
+    public void updateAdapter(ArrayList<Conta> bills) {
+        if (mBillsPaid != null && bills != null) {
+            mBillsPaid.clear();
+            mBillsPaid = bills;
+        }
+        notifyDataSetChanged();
+    }
+
+	public List<Conta> getListBillSelected() {
+		return listBillSelected;
+	}
+
+	public void setListBillSelected(List<Conta> listBillSelected) {
+		this.listBillSelected = listBillSelected;
+	}
+    
+}
